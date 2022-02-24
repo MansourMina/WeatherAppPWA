d5afd7ae13c05c104217b612f0ff29cafdceb376
MansourMina <mansourmina10@gmail.com>
2022-02-24 13:48:40 +0100

    Favorite page

--- dist/precache-manifest.cbd110a9fafcbff2f8cdf0f4f7648a38.js
+++ dist/precache-manifest.cbd110a9fafcbff2f8cdf0f4f7648a38.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1b0b770c653451ff0a69",
-    "url": "/css/app.a06ea123.css"
+    "revision": "84a5fb35710029f05870",
+    "url": "/css/app.5fac12ec.css"
   },
   {
-    "revision": "4406172b26260373139f",
-    "url": "/css/chunk-vendors.efd62e4e.css"
+    "revision": "3330d7449f24a42f658e",
+    "url": "/css/chunk-vendors.07af6cdd.css"
   },
   {
-    "revision": "be7b3b42af37fb538cb3dd6813b9d6ec",
+    "revision": "52689acdadbf9215c9b2c88a5fcba09f",
     "url": "/index.html"
   },
   {
-    "revision": "1b0b770c653451ff0a69",
-    "url": "/js/app.5c7f4284.js"
+    "revision": "84a5fb35710029f05870",
+    "url": "/js/app.182860a7.js"
   },
   {
-    "revision": "4406172b26260373139f",
-    "url": "/js/chunk-vendors.67656f26.js"
+    "revision": "3330d7449f24a42f658e",
+    "url": "/js/chunk-vendors.ea202416.js"
   },
   {
     "revision": "c3339f6c132e82f60c9b78a0c1e0a5f5",
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.0a399186985b2486e4ec62411badcbdf.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.cbd110a9fafcbff2f8cdf0f4f7648a38.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* global workbox */
 if (workbox) {
